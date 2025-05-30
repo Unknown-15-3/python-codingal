4b39210601c1ac481e87dcc4cbd00b9148892bb6
Unknown-15-3 <harshinibasubarua@gmail.com>
2025-05-30 20:55:11 +0530

    lesson 3

---
+++ lesson 3/Typecasting.py
@@ -0,0 +1,19 @@
+name="penguin"
+age=22
+is_college_student=True
+weight=55.5
+print("name:",name )
+print("data type of name is", type(name))
+print("age:", age)
+print("data type of age is", type(age))
+print("is_college_student:", is_college_student)
+print("data type of is_college_student is", type(is_college_student))
+print("weight:", weight)
+print("data type of weight is", type(weight))
+print("after typecasting... \n")
+age = str(age)
+print(age)
+print("data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("data type of weight is", type(weight))
